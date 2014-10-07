cf967ae188503bbb76d36669f979ade01052a310
IreneGuerra <i.gferreras@alumnos.upm.es>
2014-10-07 13:58:44 +0200

    Ultimas modificaciones.

---
+++ src/test/java/es/upm/miw/ecp1patrones/vehiculo/CocheTest.java
@@ -0,0 +1,27 @@
+package es.upm.miw.ecp1patrones.vehiculo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CocheTest {
+    
+    private Vehiculo coche = new Coche();
+    
+    @Before
+    public void before(){
+        
+    }
+
+    @Test
+    public void testCalcularPrecio() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/test/java/es/upm/miw/ecp1patrones/vehiculo/GestorVehiculoTest.java
@@ -0,0 +1,27 @@
+package es.upm.miw.ecp1patrones.vehiculo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GestorVehiculoTest {
+    
+    private GestorVehiculo gestorVehiculo;
+    
+    @Before
+    public void before(){
+        this.gestorVehiculo = new GestorVehiculo();
+    }
+
+    @Test
+    public void testAniadirBuscarVehiculo() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString() {
+        fail("Not yet implemented");
+    }
+
+}
