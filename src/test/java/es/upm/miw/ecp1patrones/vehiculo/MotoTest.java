d11230438a2a921af0a0bda223b3f5bfd037b357
Irene1612 <vicmatil.igf@gmail.com>
2014-10-11 18:49:03 +0200

    Test de la clase Moto.

---
+++ src/test/java/es/upm/miw/ecp1patrones/vehiculo/MotoTest.java
@@ -0,0 +1,29 @@
+package es.upm.miw.ecp1patrones.vehiculo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MotoTest {
+
+	private Vehiculo moto;	
+
+	@Before
+	public void before() {
+		moto = new Moto("id1", "220cc");
+	}
+
+	@Test
+	public void testCalcularPrecio() {
+		assertEquals(8.0, this.moto.calcularPrecio(1), 10e-3);
+		assertEquals(24.0, this.moto.calcularPrecio(3), 10e-3);
+		assertEquals(56.0, this.moto.calcularPrecio(7), 10e-3);
+		assertEquals(56.0, this.moto.calcularPrecio(8), 10e-3);		
+	}
+
+	@Test
+	public void testToString() {
+		assertEquals("Moto: [id: id1], [descripcion: 220cc]", moto.toString());
+	}
+}
